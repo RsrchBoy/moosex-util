a0c2e04d531b5a9413c7866752b9b7e8ced07e42
Chris Weyl <cweyl@alumni.drew.edu>
2014-03-03 11:40:25 -0800

    Build results of 8892ab2 (on master)

--- lib/MooseX/Util/Meta/Class.pm
+++ lib/MooseX/Util/Meta/Class.pm
@@ -1,5 +1,17 @@
+#
+# This file is part of MooseX-Util
+#
+# This software is Copyright (c) 2012 by Chris Weyl.
+#
+# This is free software, licensed under:
+#
+#   The GNU Lesser General Public License, Version 2.1, February 1999
+#
 package MooseX::Util::Meta::Class;
-
+BEGIN {
+  $MooseX::Util::Meta::Class::AUTHORITY = 'cpan:RSRCHBOY';
+}
+$MooseX::Util::Meta::Class::VERSION = '0.002';
 # ABSTRACT: A helper metaclass
 
 use Moose;
@@ -12,7 +24,22 @@ with 'MooseX::TraitFor::Moose::Meta::Class::BetterAnonClassNames';
 # NOTE: making this package immutable breaks our metaclass compatibility!
 #__PACKAGE__->meta->make_immutable;
 !!42;
+
 __END__
+
+=pod
+
+=encoding UTF-8
+
+=for :stopwords Chris Weyl
+
+=head1 NAME
+
+MooseX::Util::Meta::Class - A helper metaclass
+
+=head1 VERSION
+
+This document describes version 0.002 of MooseX::Util::Meta::Class - released March 03, 2014 as part of MooseX-Util.
 
 =head1 SYNOPSIS
 
@@ -31,5 +58,43 @@ __END__
 This is a trivial extension of L<Moose::Meta::Class> that consumes the
 L<BetterAnonClassNames|MooseX::TraitFor::Moose::Meta::Class::BetterAnonClassNames>
 trait.
+
+=head1 SEE ALSO
+
+Please see those modules/websites for more information related to this module.
+
+=over 4
+
+=item *
+
+L<MooseX::Util|MooseX::Util>
+
+=back
+
+=head1 SOURCE
+
+The development version is on github at L<http://github.com/RsrchBoy/moosex-util>
+and may be cloned from L<git://github.com/RsrchBoy/moosex-util.git>
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website
+https://github.com/RsrchBoy/moosex-util/issues
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Chris Weyl <cweyl@alumni.drew.edu>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2012 by Chris Weyl.
+
+This is free software, licensed under:
+
+  The GNU Lesser General Public License, Version 2.1, February 1999
 
 =cut
